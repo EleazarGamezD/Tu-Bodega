547f4c47f0b0f64d6192a9a2c568dbecd9c11dc1
Eleazar Gamez <82742317+froitzent@users.noreply.github.com>
2023-08-30 14:29:41 -0500

    fixing order controller

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import { OrdersController } from './Orders.Controller';
+import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
